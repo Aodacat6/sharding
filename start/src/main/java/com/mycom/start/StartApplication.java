f133f8fcdd079fa489e38007a39225b6c514fa43
songdalin <songdalin@qdcares.cn>
2022-03-29 11:10:21 +0800

    项目启动

---
+++ start/src/main/java/com/mycom/start/StartApplication.java
@@ -0,0 +1,20 @@
+package com.mycom.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：songdalin
+ * @date ：2022-3-29 上午 10:56
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
